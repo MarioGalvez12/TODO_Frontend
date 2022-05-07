671f242a03964a102b6180ce195d21804db286a0
Mario Galvez <mariogalvez253@gmail.com>
2022-05-07 17:45:31 -0500

    login y sign up completo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-
 router.route("/").get((req,res)=>{
     res.render("home", {
+        API_URL: process.env.API_URL
+    });
+})
+router.route("/tasks").get((req,res)=>{
+    res.render("tasks", {
+        layout: "tasksLayout",
         API_URL: process.env.API_URL
     });
 })
