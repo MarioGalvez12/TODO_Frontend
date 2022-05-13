2f4819d0f0d0b75530c63c1d938ece1e335fcf1c
casillas-m <martin.casillas.ramos@gmail.com>
2022-05-13 15:52:41 -0500

    Edit task added

--- src/routes/router.js
+++ src/routes/router.js
@@ -25,20 +25,31 @@ router.route("/tasks").get(authMiddleware.authMiddleware, (req, res) => {
             switch (status) {
                 case 200:
                     const monthNames = ["January", "February", "March", "April", "May", "June",
-                    "July", "August", "September", "October", "November", "December"
+                        "July", "August", "September", "October", "November", "December"
                     ];
                     let tasks = JSON.parse(result).tasks
                     tasks.forEach(e => {
+                        let milliseconds = parseInt((e.timeEstimate % 1000) / 100),
+                            seconds = Math.floor((e.timeEstimate / 1000) % 60),
+                            minutes = Math.floor((e.timeEstimate / (1000 * 60)) % 60),
+                            hours = Math.floor((e.timeEstimate / (1000 * 60 * 60)) % 24);
+
+                        hours = (hours < 10) ? "0" + hours : hours;
+                        minutes = (minutes < 10) ? "0" + minutes : minutes;
+                        seconds = (seconds < 10) ? "0" + seconds : seconds;
+                        let timeEstimateText = hours + ":" + minutes;
+                        e.timeEstimateText = timeEstimateText;
                         let createdAt = new Date(e.createdAt)
                         e.createdAt = `${monthNames[createdAt.getMonth()]} ${createdAt.getDate()} ${createdAt.getFullYear()}`
-                        if(e.starTime>0){
+                        if (e.starTime > 0) {
                             let starTime = new Date(e.starTime)
                             e.starTime = `${monthNames[starTime.getMonth()]} ${starTime.getDate()} ${starTime.getFullYear()}`
                         }
-                        if(e.endTime>0){
+                        if (e.endTime > 0) {
                             let endTime = new Date(e.endTime)
                             e.endTime = `${monthNames[endTime.getMonth()]} ${endTime.getDate()} ${endTime.getFullYear()}`
                         }
+                        e.all = JSON.stringify(e)
                     });
                     res.render("tasks", {
                         layout: "tasksLayout",
